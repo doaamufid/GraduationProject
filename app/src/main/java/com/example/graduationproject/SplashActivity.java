483ab45b2515265ea7afca1fcd154ca5f113aac7
DoaaMufid2003 <your_email@example.com>
2026-06-24 14:42:01 +0300

    Design of the first two screens

--- app/src/main/java/com/example/graduationproject/SplashActivity.java
+++ app/src/main/java/com/example/graduationproject/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.graduationproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.graduationproject.databinding.ActivitySplashBinding;
+
 public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // بعد 3 ثواني روح على SplashSelectActivity
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, SplashSelectActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
     }
 }
